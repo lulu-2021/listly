511a8422936b57428004665b219c6e082cd996de
Steve Forkin <steve.forkin@gmail.com>
2015-03-28 22:52:00 +1100

    added a railtie class and the base_list that all listly lists will inherit their functionality from

--- lib/listly.rb
+++ lib/listly.rb
@@ -1,5 +1,19 @@
-require "listly/version"
-
+#
+require File.dirname(__FILE__) + '/listly/base_list'
+require File.dirname(__FILE__) + '/listly/railtie' if defined? ::Rails::Railtie
+#
+# The config class allows the Rails Application to store configuration about the gem
+#
 module Listly
-  # Your code goes here...
+  class Config
+    attr_accessor :listly_store_location
+  end
+
+  def self.config
+    @@config ||= Config.new
+  end
+
+  def self.configure
+    yield self.config
+  end
 end
---
+++ lib/listly/base_list.rb
@@ -0,0 +1,46 @@
+module BaseList
+
+  # - creates items of the passed in item_class with the passed in type_prefix for code and name for list items
+  def all_list_items(type_prefix, item_class, list_items_hash = {})
+    list_items = []
+    list_items_hash.each do |item|
+      data = {}
+      item.each{ |key, value| data["#{type_prefix}_#{key}"] = "#{value}" }
+      list_items << Module.const_get(item_class).new(data)
+    end
+    list_items
+  end
+
+  def list_item_name(all_items, code_field, name_field, list_item_code)
+    name = ''
+    all_items.map do |item|
+      if (item.send code_field) == list_item_code
+        name = item.send name_field
+      end
+    end
+    name
+  end
+
+  class MyListType
+    attr_accessor :list_hash # - this is for convenience methods in Rails
+
+    # - dynamically create the properties from the passed in hash at runtime
+    define_method :initialize do |args|
+      @list_hash = args
+
+      args.each do |name, value|
+        instance_variable_get("@#{name}")
+        instance_variable_set("@#{name}", value)
+      end
+    end
+
+    # - these are for convenience methods in Rails i.e. make lists behave like AR objects
+    def [](key)
+      list_hash[key.to_s]
+    end
+
+    def []=(key, value)
+      list_hash[key.to_s] = value
+    end
+  end
+end
